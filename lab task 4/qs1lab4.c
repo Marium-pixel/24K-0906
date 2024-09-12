4517007d79755cc2e8587042671db1808f2c1945
Marium-pixel <imranmarium742@gmail.com>
2024-09-12 14:52:31 +0500

    qs 1 of lab 4

---
+++ lab task 4/qs1lab4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+
+int num;
+printf("enter number");
+scanf(" %d", &num );
+if (num%3==0){
+printf("number is multiple of 3");
+}
+else if(num%3!=0){
+printf("number is not multiple of 3");
+
+}
+}
