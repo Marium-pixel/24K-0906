81e809b6cf519ab1c654fbe54e6cd0ea6bc0075d
Marium-pixel <imranmarium742@gmail.com>
2024-09-26 09:47:01 +0500

    Create task3.c

---
+++ lab 5/task3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	char type,coffee[50];
+	int cupsize;
+	printf("enter type of coffee you want : b for black or w for white \n");
+	scanf("%c",&type);
+	printf("enter y-yes or n-no if coffee is manual \n");
+	scanf("%s",&coffee );
+	printf("enter 1 for double 0 for other \n");
+	scanf("%d",&cupsize);
+	if ( type == 'b' || type == 'B' && coffee=='y'){
+		
+	}
+	
+}
