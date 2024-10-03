4c22d25e0048e1f1fa3d38aeffa5af7a3ee89eb9
Marium-pixel <imranmarium742@gmail.com>
2024-10-03 14:49:14 +0500

    Create qs2.c

---
+++ lab 6/qs2.c
@@ -0,0 +1,10 @@
+//incoreect
+#include<stdio.h>
+int main(){
+	int num;
+	printf("enter a number");
+	scanf("%d",&num);
+	if((num/2!=0) && (num/3 !=0) && (num/5!=0)){
+		printf(" number is prime");
+	}
+}
