4cdbb1cfa6c6d7e28db8eaeb425abadfe0930819
Marium-pixel <imranmarium742@gmail.com>
2024-10-09 23:11:36 +0500

    Create Qs7.c

---
+++ lab 6/Qs7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int i;
+	for(i=1;i<7;i++){
+		if(i==1){
+		printf("A  B  C  D  E\n");
+		}
+		else if(i==3){
+		printf("C           G\n");
+		}
+		else if(i==4){
+				printf("D           H\n");
+		}
+		else if(i==5){
+				printf("E           I\n");
+		}
+		else if(i==6){
+		        printf("F  G  H  I  J\n");			
+		}	
+	}
+	return 0;
+}
