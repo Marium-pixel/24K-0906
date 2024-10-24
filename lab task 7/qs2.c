7661295c60979be663a4d3554c1930761c62a384
Marium-pixel <imranmarium742@gmail.com>
2024-10-24 23:26:20 +0500

    Create qs2.c

---
+++ lab task 7/qs2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int i,count=0;
+	char inp[10];
+	printf("enter any words");
+	scanf("%c",inp);
+	for(i=0;i<=10;i++)
+	{
+		if(inp=='a'||inp=='e'||inp=='i'||inp=='o'||inp=='u'){
+			count=count+1;
+			
+		}
+	printf("the total amount of vowels are %d",vowel);	
+	}
+
+	
+
+
+}
