b03acd11aeee7104c91c0764678b1e75e388e023
Marium-pixel <imranmarium742@gmail.com>
2024-10-25 20:41:33 +0500

    Update qs2.c

--- lab task 7/qs2.c
+++ lab task 7/qs2.c
@@ -1,19 +1,32 @@
 #include<stdio.h>
 int main(){
 	int i,count=0;
-	char inp[10];
-	printf("enter any words");
-	scanf("%c",inp);
-	for(i=0;i<=10;i++)
-	{
-		if(inp=='a'||inp=='e'||inp=='i'||inp=='o'||inp=='u'){
-			count=count+1;
-			
-		}
-	printf("the total amount of vowels are %d",vowel);	
-	}
-
+	char inp[100];
+	printf("enter any words\n");
+	scanf("%s",inp);
+	printf("enter amount of letters\n");
+	int letternum;
+	scanf("%d",&letternum);
+	int counta=0,counte=0,counti=0,counto=0,countu=0;
 	
+	for (i=0;i<=letternum;i++)
+	{ if (inp[i]=='a'){
+		counta=counta +1;
+	}
+	else if (inp[i]=='e'){
+		counte=counte+1;
+	}
+	else if (inp[i]=='i'){
+		counti=counti+1;
 
+	}else if (inp[i]=='o'){
+		counto=counto+1;
 
+	}else if (inp[i]=='u'){
+		countu=countu+1;
+	}
+	 
+	}
+	printf("number of a is %d\n e is%d i is %d\n o is %d\n u is %d\n",counta,counte,counti,counto,countu);
+	
 }
