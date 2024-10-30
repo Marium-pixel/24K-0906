7b96f33f59a48ca2a171576d3993812506118132
Marium-pixel <imranmarium742@gmail.com>
2024-10-31 00:29:01 +0500

    Update qs7.c

--- lab 8/qs7.c
+++ lab 8/qs7.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
     char arr[5][105] = {};  
-    int i = 4, j = 0, up=1,x=1, count = 0;
+    int i = 4, j = 0,upcount=1,x=1, count = 0;
     while (count < 3) {
         arr[i][j] = '*';
-        up ? (i--, j +=x++) : (i++, j +=x--);
-        if (i == 0 && up) {
-            up = 0;
+        upcount? (i--, j +=x++) : (i++, j +=x--);
+        if (i == 0 && upcount) {
+            upcount = 0;
             x--;
-        } else if (i == 4 && !up) {
-            up = 1;
+        } else if (i == 4 && !upcount) {
+            upcount = 1;
             x = 1;
             count++;
             arr[i][j] = '*';
