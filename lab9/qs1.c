d8d7b329a8fb0e9f8f7b377030a7973d45f81be8
Marium-pixel <imranmarium742@gmail.com>
2024-10-31 15:20:25 +0500

    Update qs1.c

--- lab9/qs1.c
+++ lab9/qs1.c
@@ -2,29 +2,32 @@
 #include<string.h>
 
 int main(){
+	int rows = 5;
+	int cols = 20;
 	char input[5][20];
 	char input2[5][20];
-	int i,j,legnth;
-	for(i=0;i<5;i++){
+	int i,j;
+	for(i=0;i<rows;i++){
 		printf("enter string %d\n",i+1);
-		scanf("%s",&input[i][j]);
-	
+		gets(input[i]);
 	}
-	int result;
+
 	for(i=0;i<5;i++){
-	strcpy(input2[i],input[i]);
+	printf("string %d: %s\n",i+1, input[i]);
+    strcpy(input2[i],input[i]);
 	strrev(input2[i]);
+
+    int result=strcmp(input2[i],input[i]);
+	
+	
+	if(result==0){
+		printf("this is a palindrome\n");
+	}
+	else{
+		printf("not a palindrome\n");
+	}
+
+
 }
-    if(input2[i]==input[i]){
-		result=0;
-	}
-	if(result=0){
-		printf("thisis a palindrome");
-	}
-	else if (result != 0){
-		printf("not a palindrome");
-	}
-
-
 	
 }
